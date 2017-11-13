a9105126a4a02b145b29ccca6b4d8b46e2ed9c4c
a.honcharov <a.honcharov>
2017-11-13 18:51:25 +0200

    adding Portal

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Popup from './Popup'
 import { connect } from 'react-redux'
-import logo from '../assets/logo.svg';
+
+import logo from '../assets/logo.svg'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {showPopup: false}
+
+    this.handleShow = this.handleShow.bind(this)
+    this.handleHide = this.handleHide.bind(this)
+  }
+
+  handleShow(e) {
+    e.preventDefault()
+    this.setState({ showPopup:true })
+  }
+  handleHide(e) {
+    e.preventDefault()
+    this.setState({ showPopup:false })
+  }
+
+  showPopup() {
+    return (
+      this.state.showPopup
+        ? <Popup>
+            <span className={this.props.clasName || 'title'}>Hey from Popup</span>
+            <button onClick={this.handleHide}>Hide</button>
+          </Popup>
+        : null
+    )
+  }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +44,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.handleShow}>Show Popup</button>
+        <div id='popup-container'>{this.showPopup()}</div>
       </div>
     );
   }
---
+++ src/components/Popup.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { connect } from 'react-redux'
+import './Popup.scss'
+
+class Popup extends Component {
+  constructor(props) {
+    super(props)
+
+    this.el = document.createElement('div')
+    this.el.setAttribute('id', 'popup')
+  }
+
+  componentDidMount() {
+    document.querySelector('#popup-container').appendChild(this.el)
+  }
+  componentWillUnmount() {
+    document.querySelector('#popup-container').removeChild(this.el)
+  }
+
+  render() {
+    return ReactDOM.createPortal(this.props.children, this.el)
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    // config: state.config,
+    // menu: state.menu
+  }}
+
+export default connect(mapStateToProps, {})(Popup)
