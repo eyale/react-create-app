f6c445e6c0872ed36e2a3e05270960c8da81a86a
pennywise <pennywise@syneforge.com>
2018-02-12 18:32:33 +0200

    checking the quotes after prettier

--- src/redux/modules/check.js
+++ src/redux/modules/check.js
@@ -1,12 +1,12 @@
-const ON_CHANGE = "ON_CHANGE";
+const ON_CHANGE = 'ON_CHANGE';
 
 export const inputOnChangeAction = e => ({
   type: ON_CHANGE,
   payload: e.target.value
 });
 
-const inputReducer = (state = "", action) => {
-  if (Object.is(action.type, ON_CHANGE)) {
+const inputReducer = (state = '', action) => {
+  if (action.type === ON_CHANGE) {
     return action.payload;
   }
 
