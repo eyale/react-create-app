6b8257d0b0134182dec16d6bfe5eab8793bb29ac
pennywise <pennywise@syneforge.com>
2018-02-12 18:57:54 +0200

    adding the axios and sagas starting

---
+++ src/redux/saga/sagas.js
@@ -0,0 +1,24 @@
+import {takeLatest, call, put} from 'redux-saga/effects';
+import axios from 'axios';
+
+export function* watcherSaga() {
+  yield takeLatest('API_CALL_REQUEST', workerSaga);
+}
+
+function fetchDog() {
+  return axios({
+    method: 'get',
+    url: 'https://dog.ceo/api/breeds/image/random'
+  });
+}
+
+function* workerSaga() {
+  try {
+    const resp = yield call(fetchDog);
+    const dog = resp.data.message;
+
+    yield put({type: 'API_CALL_SUCCESS', dog});
+  } catch (error) {
+    yield put({type: 'API_CALL_FAILURE', error});
+  }
+}
