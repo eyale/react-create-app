a12ae66e2ee4b7c16c1ff6d2f4ce6b5fcb4afbc1
pennywise <pennywise@syneforge.com>
2018-02-15 11:23:44 +0200

    staging the theme in localstorage

--- src/redux/modules/theme.js
+++ src/redux/modules/theme.js
@@ -1,16 +1,19 @@
 const THEME_ON_CHANGE = 'THEME_ON_CHANGE';
-
-export const themeOnChangeAction = e => ({
-  type: THEME_ON_CHANGE,
-  payload: e.target.value
-});
-
 const initialState = {
   isLight: true
 };
 
+if (window.localStorage.getItem('isLight') !== null) {
+  initialState.isLight = window.localStorage.getItem('isLight') === 'true';
+}
+
+if (window.localStorage.getItem('isLight') === null) {
+  window.localStorage.setItem('isLight', initialState.isLight);
+}
+
 export const themeReducer = (state = initialState, action) => {
   if (action.type === THEME_ON_CHANGE) {
+    window.localStorage.setItem('isLight', !state.isLight);
     return {...state, isLight: !state.isLight};
   }
 
